6243adf616ff445fa517e1d17e6f32f07ec4f856
marianmarcu <mrc.adrian@yahoo.com>
2023-10-17 00:03:01 +0100

    check for letters and negative input

--- src/EventSeatingPlanner.java
+++ src/EventSeatingPlanner.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class EventSeatingPlanner {
 
@@ -15,7 +16,20 @@ public class EventSeatingPlanner {
             System.out.println("2. Create seating plan");
             System.out.println("3. Exit");
             System.out.print("Enter a choice: ");
-            int choice = scanner.nextInt();
+
+            int choice = -1;
+            while (true) {
+                try {
+                    choice = scanner.nextInt();
+                    if (choice < 0) {
+                        throw new InputMismatchException("Negative number detected.");
+                    }
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input. Please enter a non-negative number.");
+                    scanner.next();
+                }
+            }
 
             switch (choice) {
                 case 1:
@@ -39,25 +53,47 @@ public class EventSeatingPlanner {
     }
 
     public static int getAttendeesCount(Scanner scanner) {
-        System.out.print("Enter the total number of people attending the event: ");
-        return scanner.nextInt();
+        while (true) {
+            try {
+                System.out.print("Enter the total number of people attending the event: ");
+                int attendees = scanner.nextInt();
+                if (attendees < 0) {
+                    throw new InputMismatchException("Negative number detected.");
+                }
+                return attendees;
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a non-negative number.");
+                scanner.next();
+            }
+        }
     }
 
     public static void getGroupCounts(Scanner scanner, int totalAttendees, int[] groupCounts) {
         int attendeesAssigned = 0;
         for (int i = 0; i < GROUP_SIZES.length && attendeesAssigned < totalAttendees; i++) {
-            System.out.print("Enter the number of groups of size " + GROUP_SIZES[i] + ": ");
-            groupCounts[i] = scanner.nextInt();
+            while (true) {
+                try {
+                    System.out.print("Enter the number of groups of size " + GROUP_SIZES[i] + ": ");
+                    groupCounts[i] = scanner.nextInt();
+                    if (groupCounts[i] < 0) {
+                        throw new InputMismatchException("Negative number detected.");
+                    }
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input. Please enter a non-negative number.");
+                    scanner.next();
+                }
+            }
             attendeesAssigned += groupCounts[i] * GROUP_SIZES[i];
         }
 
         if (attendeesAssigned != totalAttendees) {
             System.out.println("Error: The sum of attendees in groups doesn't match the total attendees.");
         } else {
-            displayGroupDetails(groupCounts);  // Display details immediately after input.
+            displayGroupDetails(groupCounts);
             System.out.println("Press Enter to return to the main menu.");
-            scanner.nextLine();  // consume the leftover newline
-            scanner.nextLine();  // wait for user to press Enter
+            scanner.nextLine();
+            scanner.nextLine();
         }
     }
 
@@ -78,41 +114,35 @@ public class EventSeatingPlanner {
         int tablesOf6 = 0;
         int tablesOf8 = 0;
 
-        // Handle groups of size 6 and 5 first
-        tablesOf6 += groupCounts[4];  // Groups of 6
-        tablesOf6 += groupCounts[3];  // Groups of 5
+        tablesOf6 += groupCounts[4];
+        tablesOf6 += groupCounts[3];
         groupCounts[4] = 0;
         groupCounts[3] = 0;
 
-        // Pair groups of 4 with groups of 2, if possible
         while (groupCounts[2] > 0 && groupCounts[0] > 0) {
             tablesOf8++;
             groupCounts[2]--;
             groupCounts[0]--;
         }
 
-        // Pair groups of 3 together
         while (groupCounts[1] > 1) {
             tablesOf8++;
             groupCounts[1] -= 2;
         }
 
-        // Place remaining groups of 4 on tables of 8
         while (groupCounts[2] > 0) {
             tablesOf8++;
             groupCounts[2]--;
         }
 
-        // Pair remaining groups of 3 with groups of 2
         while (groupCounts[1] > 0 && groupCounts[0] > 0) {
             tablesOf8++;
             groupCounts[1]--;
             groupCounts[0]--;
         }
 
-        // Allocate remaining groups
-        tablesOf6 += groupCounts[1];  // Groups of 3
-        tablesOf8 += (int) Math.ceil(groupCounts[0] / 2.0);  // Groups of 2
+        tablesOf6 += groupCounts[1];
+        tablesOf8 += (int) Math.ceil(groupCounts[0] / 2.0);
 
         System.out.println("Tables of 6 needed: " + tablesOf6);
         System.out.println("Tables of 8 needed: " + tablesOf8);
